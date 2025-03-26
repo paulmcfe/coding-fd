d346543466c97de0a46488dc47476ddfa823d7c0
Paul McFedries <pmcfedries@gmail.com>
2025-03-26 15:53:43 -0400

    Add Chapter 4 folder

---
+++ Chapter04/whats_your_name.py
@@ -0,0 +1,11 @@
+# Prompt the user for their name
+name = input("What's your name? ")
+
+# Print a friendly greeting
+print("Nice to meet you, " + name + "!")
+
+# Calculate the number of characters in the user's name
+chars = len(name)
+
+# Print the number of characters in the user's name
+print("Your name contains " + str(chars) + " characters.")

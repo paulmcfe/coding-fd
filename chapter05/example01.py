706a507a97eddd6fa095f50633547462c7dc2afb
Paul McFedries <pmcfedries@gmail.com>
2025-04-03 15:49:35 -0400

    Add chapter 5 examples

---
+++ chapter05/example01.py
@@ -0,0 +1,4 @@
+cookies_eaten = 3
+cookies_not_eaten = 2
+total_cookies = cookies_eaten + cookies_not_eaten
+print(total_cookies)
---
+++ chapter05/example02.py
@@ -0,0 +1,6 @@
+hungry = True
+has_cookies = False
+if hungry and has_cookies:
+    print("Time to munch!")
+else:
+    print("No cookies? Tragedy.")

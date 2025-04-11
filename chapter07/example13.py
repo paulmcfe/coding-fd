4f522457838a7d1d2f8b97baca96497111d56e2c
Paul <mail@mcfedries.com>
2025-04-11 16:13:04 -0400

    Add final Chapter 7 examples

---
+++ chapter07/example13.py
@@ -0,0 +1,8 @@
+# Prompt for an age
+age = input("How old are you? ")
+
+# Print the age variable value
+print(f"The value of age is {age}.")
+
+age_next_year = int(age) + 1
+print(f"Next year, you'll be {age_next_year}!")
---
+++ chapter07/example14.py
@@ -0,0 +1,8 @@
+# Prompt for an age
+age = input("How old are you? ")
+
+# Print the age variable data type
+print(f"The data type of age is {type(age)}")
+
+age_next_year = int(age) + 1
+print(f"Next year, you'll be {age_next_year}!")
